3d74da52d028ff10178e2011042dd30b8376b637
Mujthaba Rahman <mujthabarahman.m@gmail.com>
2023-01-22 13:52:39 +0530

    add dynamic data products in user side

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,13 @@
 
 var express = require('express');
+const productHelpers = require('../helpers/product-helpers');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('users/all-products');
+  productHelpers.getAllProducts().then((products)=>{
+    res.render('users/all-products',{products});
+  })
 });
 
 module.exports = router;
