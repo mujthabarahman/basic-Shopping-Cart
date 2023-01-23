6bc718672aeb6517e0e470fd5cbb9e74039def36
Mujthaba Rahman <mujthabarahman.m@gmail.com>
2023-01-23 15:50:50 +0530

    add router to signup page

--- routes/users.js
+++ routes/users.js
@@ -9,5 +9,8 @@ router.get('/', function(req, res, next) {
     res.render('users/all-products',{products});
   })
 });
+router.get('/signup',(req,res)=>{
+  res.render('users/signup-page')
+})
 
 module.exports = router;
